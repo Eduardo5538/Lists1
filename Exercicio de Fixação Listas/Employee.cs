5852b69cbf791dd142b91ab9918b76d107b82f73
Eduardo Corrêa <Eduardo-correa1@outlook.com.br>
2020-04-07 12:28:57 -0300

    Projeto Finalizado, armazenando dados de pessoas que alugaram um quarto

---
+++ Exercicio de Fixação Listas/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio_de_Fixação_Listas
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            ID = id;
+            Name = name;
+            Salary = salary;
+        }
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100 ;
+        }
+        public override string ToString()
+        {
+            return $"{ID}, {Name}, $ {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
